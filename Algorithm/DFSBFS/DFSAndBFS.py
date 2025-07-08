7d8bdd835f3fb8a5e8516ff8e524dc2c02567b1f
junho <jjunho891818@gmail.com>
2025-07-08 17:29:03 +0900

    Modify

--- Algorithm/DFSBFS/DFSAndBFS.py
+++ Algorithm/DFSBFS/DFSAndBFS.py
@@ -10,26 +10,25 @@ for _ in range(m):
     graph[y].append(x)
 
 # DFS(Stack)
-def dfs(node, visited, result):
+def dfs_stack(node, visited, result):
     stack = [node]
     while stack:
         node = stack.pop()
         if not visited[node]:
             result.append(str(node))
             visited[node] = True
-            # If vertex are two or more, select lowest value
-            for ele in list(sorted(graph[node], reverse = True)):
+            # If vertex are two or more, select lowest value first
+            for ele in sorted(graph[node], reverse = True):
                 if not visited[ele]:
                     stack.append(ele)
     return result
 # DFS(Recursive)
-def dfs(node, visited, result):
+def dfs_rec(node, visited, result):
     result.append(str(node))
     visited[node] = True
-    for ele in list(sorted(graph[node])):
+    for ele in sorted(graph[node]):
         if not visited[ele]:
-            dfs(ele, graph, visited, result)
-    return result
+            dfs_rec(ele, visited, result)
 # BFS
 def bfs(node, visited, result):
     queue = deque()
@@ -38,11 +37,15 @@ def bfs(node, visited, result):
     while queue:
         node = queue.popleft()
         result.append(str(node))
-        for ele in list(sorted(graph[node])):
+        for ele in sorted(graph[node]):
             if not visited[ele]:
                 queue.append(ele)
                 visited[ele] = True
     return result
 
-print(" ".join(dfs(v, [False] * (n + 1), [])))
+rec_result = []
+dfs_rec(v, [False] * (n + 1), rec_result)
+
+print(" ".join(dfs_stack(v, [False] * (n + 1), [])))
+# print(" ".join(rec_result))
 print(" ".join(bfs(v, [False] * (n + 1), [])))
